124d9af01e6020905c1d9c0ee673981ce78d4358
Samuel <fridaydamilare22@gmail.com>
2022-09-17 12:06:51 -0700

    Uploading

---
+++ main.js
@@ -0,0 +1,19 @@
+const container = document.querySelector(".container");
+const generate = document.querySelector(".form button")
+const qrInput = document.querySelector(".form input")
+const qrImg = document.querySelector(".qrCode img")
+
+generate.addEventListener("click", () =>{
+    const input = qrInput.value;
+    if(!input) return false
+
+    qrImg.src = `https://api.qrserver.com/v1/create-qr-code/?size=170x170&data=${input}`
+
+    generate.textContent = "Generate QR Code..."
+
+    qrImg.addEventListener("load", () =>{
+        container.classList.add("active")
+        generate.textContent = "Generating QR Code"
+    })
+    
+})
